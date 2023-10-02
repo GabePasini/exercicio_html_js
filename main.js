4d344c3dd4ab0241480557742d1de8640b9bd9eb
GabePasini <138173180+GabePasini@users.noreply.github.com>
2023-10-02 19:07:26 -0300

    Right archives

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
 const form = document.getElementById('validation');
-const fieldA = document.getElementById('field-A').value;
-const fieldB = document.getElementById('field-B').value;
-let formEValido = false;
+var fieldA = document.getElementById('field-A');
+var fieldB = document.getElementById('field-B');
 
-if (fieldA > fieldB) {
-    document.querySelector('.error-message').style.display ='block';
-}
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const valorA = parseFloat(fieldA.value);
+    const valorB = parseFloat(fieldB.value);
+
+    const mensagemSucesso = `O segundo valor (${valorB}) é maior que o primeiro (${valorA})`;
+    const mensagemErro = `O segundo valor (${valorB}) deve ser maior que o primeiro (${valorA}), insira outros valores`;
+
+    if (!isNaN(valorA) && !isNaN(valorB)) {
+        if (valorB > valorA) {
+            const containerMensagemSucesso = document.querySelector('.success-message');
+            containerMensagemSucesso.innerHTML = mensagemSucesso;
+            containerMensagemSucesso.style.display = 'block';
+
+            fieldA.value = '';
+            fieldB.value = '';
+        } else {
+            const containerMensagemErro = document.querySelector('.error-message');
+            containerMensagemErro.innerHTML = mensagemErro;
+            containerMensagemErro.style.display = 'block';
+        }
+    }
+});
+
+fieldB.addEventListener('keyup', function() {
+    const valorA = parseFloat(fieldA.value);
+    const valorB = parseFloat(fieldB.value);
+
+    if (!isNaN(valorA) && !isNaN(valorB)) {
+        if (valorB <= valorA) {
+            fieldB.style.border = '1px solid red';
+            document.querySelector('.error-message').style.display = 'block';
+        } else {
+            fieldB.style.border = '';
+            document.querySelector('.error-message').style.display = 'none';
+        }
+    }
+});
